035327e566f68c223861f66d4ecaa4eac9647150
sowmyay <syellapragada3@gatech.edu>
2017-10-13 00:45:45 -0400

    Adding files

---
+++ resQ/Controller/MakeReqController.swift
@@ -0,0 +1,35 @@
+//
+//  MakeReqController.swift
+//  ResQ
+//
+//  Created by sowmya yellapragada on 10/8/17.
+//  Copyright © 2017 sowmya.yellapragada. All rights reserved.
+//
+
+import UIKit
+
+class MakeReqController: BaseController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
